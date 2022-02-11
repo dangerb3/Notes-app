28dc6f2790952ec9990a8974a5292126d214868b
dangerb3 <43404068+dangerb3@users.noreply.github.com>
2022-02-11 12:01:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-// Альтернатива JSX. Но код получается громоздкий, поэтому лучше JSX
-// ReactDOM.render(
-//   React.createElement('button', {
-//       disabled:false, onClick: ()=> console.log('click')
-//   }, 'test'),
-//   document.getElementById('root')
-// );
-
 ReactDOM.render(
     <App />,
       document.getElementById('root')
