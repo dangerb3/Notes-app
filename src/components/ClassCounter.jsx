1b711c2d8c20a7707ba4f4da97072eb7bf9a9183
dangerb3 <43404068+dangerb3@users.noreply.github.com>
2022-02-11 12:02:26 +0300

    Update ClassCounter.jsx

--- src/components/ClassCounter.jsx
+++ src/components/ClassCounter.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-// Пример создания компонента с помощью классов
-// Устаревший подход
+// Создание компонента с помощью классов
 
-class ClassCounter extends React.Component {// Классовый компонент это устаревший подход
+class ClassCounter extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
             count: 0
         }
-        this.increment = this.increment.bind(this);// Без этого не будут работать функции. Нужно забиндить контекст
+        this.increment = this.increment.bind(this);
         this.decrement = this.decrement.bind(this);// В методах класса контекст теряется.
     }
 
@@ -21,8 +20,8 @@ class ClassCounter extends React.Component {// Классовый компоне
         this.setState({count: this.state.count - 1})
     }
 
-    render(){// в Классовых компонентах возврат jsx только через функцию render(), в отличие от функциональных.
-        return (// Круглые скобки, а не фигурные, т.к. возвращаем не объект 
+    render(){
+        return (
             <div>
             <h1>{this.state.count}</h1>
             <button onClick={this.increment}>Increment</button>{/* Используем this при обращении к полям класса */}
@@ -32,4 +31,4 @@ class ClassCounter extends React.Component {// Классовый компоне
     }
 }
 
-export default ClassCounter; // Все компоненты надо экспортировать
+export default ClassCounter;
