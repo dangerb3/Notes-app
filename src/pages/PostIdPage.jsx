c2ae4befc47d5759879364c46a92e91503cdbf9a
dangerb3 <dangerb@yandex.ru>
2022-04-08 15:41:26 +0300

    Add postIdPage loading

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useFetching } from '../components/hooks/useFetching';
+import PostService from '../API/PostService';
+import { useEffect } from 'react';
+import Loader from '../components/UI/Loader/Loader';
 
 const PostIdPage = () => {
     const params = useParams();
+    const [post,setPost] = useState({});
+    const [fetchPostById, isLoading, error]  = useFetching(async (id)=>{
+        const response = await PostService.getById(id)
+        setPost(response.data)
+    })
+
+    useEffect(()=>{
+        fetchPostById(params.id)
+    },[])
     
     return (
         <div>
             <h1>Вы открыли страницу поста c id = {params.id}</h1>
+            {isLoading
+            ? <Loader/>
+            : <div>{post.id}. {post.title}</div>
+        }
         </div>
     );
 }
