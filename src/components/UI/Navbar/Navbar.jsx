86a23e679ba2962b0a3cbb9cb9718e9802e8ddc2
dangerb3 <dangerb@yandex.ru>
2022-04-11 02:34:31 +0300

    Improved Navbar

--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../../context';
 import MyButton from '../button/MyButton';
 import { useContext } from 'react';
@@ -11,13 +11,16 @@ const Navbar = () => {
     setIsAuth(false)
     localStorage.removeItem('auth')
   }
+  const navigate = useNavigate();
 
     return (
         <div className="navbar">
           <MyButton onClick={logout}>Выйти</MyButton>
           <div className="navbar__links">
-            <Link to="/about">О сайте</Link>
-            <Link to="/posts">Посты</Link>
+            {/* <Link to="/about">О сайте</Link> */}
+            <MyButton onClick={()=>navigate('/about')}>О сайте</MyButton>
+            <MyButton onClick={()=>navigate('/posts')}>Посты</MyButton>
+            {/* <Link to="/posts">Посты</Link> */}
           </div>
         </div>
     );
