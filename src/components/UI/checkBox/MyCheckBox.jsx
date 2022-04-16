deb1401414c1a15613fe8ebf3c1ada797e895c4d
dangerb3 <dangerb@yandex.ru>
2022-04-16 20:05:19 +0300

    Update MyCheckBox.jsx

--- src/components/UI/checkBox/MyCheckBox.jsx
+++ src/components/UI/checkBox/MyCheckBox.jsx
@@ -4,7 +4,6 @@ import classes from './MyCheckBox.module.css';
 function MyCheckBox({
   children, state, callback, ...props
 }) {
-  console.log(state);
   return (
   //   why not working classes.MyCheckBox.active?
     <button type="button" onClick={() => callback(state)} className={state ? classes.MyCheckBoxActive : classes.MyCheckBox} {...props}>
