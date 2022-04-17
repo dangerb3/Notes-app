1a7ac06e328689a35a34a4af32c89d3a7a59c303
dangerb3 <dangerb@yandex.ru>
2022-04-17 04:14:26 +0300

    Update PostItem.jsx

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -8,15 +8,15 @@ function PostItem(props) {
     <div className="post">
       <div className="post__content">
         <strong>
-          {props.post.id}
-          .
-          {' '}
+          {props.post.id.toString().length < 12
+            ? `${props.post.id}. `
+            : ' '}
           {props.post.title}
         </strong>
         <div>{props.post.body}</div>
       </div>
       <div className="post__btns">
-        <MyButton onClick={() => navigate(`${props.post.id}`)}>
+        <MyButton style={{ display: props.post.id.toString().length < 12 ? 'flex' : 'none' }} onClick={() => navigate(`${props.post.id}`)}>
           Open
         </MyButton>
         <MyButton onClick={() => props.remove(props.post)}>
